e5f34fa293296dda1c773c4c27a28d24e8bd4e9a
dedwards446 <150183824+dedwards446@users.noreply.github.com>
2024-02-22 11:45:40 -0600

    Update index.js
    changed root.render

--- src/index.js
+++ src/index.js
@@ -1,22 +1,20 @@
+// Import everything
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router } from 'react-router-dom';
 
 
 
 const container =  document.getElementById('root');
 const root = createRoot(container);
-const appName = "chakra-app"
 
 root.render(
-  <React.StrictMode>
-    <BrowserRouter basename={`/${appName}`}>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <Router>
+  <App />
+</Router>
 );
 
 serviceWorkerRegistration.register();
