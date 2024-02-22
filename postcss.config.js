7e9515e6944882d355e148703806640ed32e016a
dedwards446 <150183824+dedwards446@users.noreply.github.com>
2024-02-22 11:48:56 -0600

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,10 @@
+// Module exports an object configuring PostCSS plugins
 module.exports = {
+  // Define PostCSS plugins
   plugins: {
+    // Use Tailwind CSS plugin
     tailwindcss: {},
+    // Use Autoprefixer plugin
     autoprefixer: {},
   },
-}
+};
